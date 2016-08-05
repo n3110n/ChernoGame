8de88cacee4f9d422d5af0cb91b0719e7209f5e4
Nils Kahle <nils.kahle@unikik.de>
2016-08-05 17:04:54 +0200

    Lesson 008 The Screen Class

--- src/test/Game.java
+++ src/test/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import test.graphics.Screen;
+
 public class Game extends Canvas implements Runnable{
 	/**
 	 * 
@@ -23,12 +25,16 @@ public class Game extends Canvas implements Runnable{
 	private JFrame frame;
 	private boolean running = false;
 	
+	private Screen screen;
+	
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	
 	public Game(){
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
 		
 		frame = new JFrame();
 		
