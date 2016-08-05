5800d9793262993c3b91b6e7bbc537b1a1325cd6
Nils Kahle <nils.kahle@unikik.de>
2016-08-05 12:24:03 +0200

    Lesson 002 Threads

--- src/test/Game.java
+++ src/test/Game.java
@@ -1,9 +1,23 @@
 package test;
 
-public class Game {
+public class Game implements Runnable{
 	public static int width = 400;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
-
+	private Thread thread;
+	
+	public synchronized void start(){
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		try{
+			thread.join();
+		} catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
 }
