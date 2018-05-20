1c32c5e360826a504bfdc62c8811f8d68e71caa7
n3110n <neilon1@web.de>
2018-05-20 22:50:20 +0200

    EP 20
    Single Sprites loadable

---
+++ src/test/graphics/Sprite.java
@@ -0,0 +1,29 @@
+package test.graphics;
+
+public class Sprite {
+	
+	private final int SIZE;
+	private int x, y;
+	
+	public int[] pixels;
+	private SpriteSheet sheet;
+	
+	public Sprite(int size, int x, int y, SpriteSheet sheet) {
+		SIZE = size;
+		this.x = x * size;
+		this.y = y * size;
+		this.sheet = sheet;
+		pixels = new int[SIZE * SIZE];
+		
+		load();
+	}
+	
+	private void load() {
+		for(int y = 0; y < SIZE; y++) {
+			for(int x = 0; x < SIZE; x++) {
+				pixels[x+y*SIZE] = sheet.pixels[(this.x + x) + (this.y + y) * sheet.SIZE];
+			}
+		}
+	}
+	
+}
