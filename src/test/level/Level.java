756f94cc36cfab1f7d5be4cef4ef01dbefece219
n3110n <neilon1@web.de>
2018-05-21 01:10:52 +0200

    EP 24
    Level class generated as a sort of superclass

---
+++ src/test/level/Level.java
@@ -0,0 +1,37 @@
+package test.level;
+
+import test.graphics.Screen;
+
+public class Level {
+	
+	private int width, height;
+	private int[] tiles;
+	
+	public Level(int width, int height) {
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		
+		generateLevel();
+	}
+	
+	public Level(String path) {
+		loadLevel(path);
+	}
+	
+	private void loadLevel(String path) {
+		
+	}
+	
+	private void generateLevel() {
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen) {
+		
+	}
+}
