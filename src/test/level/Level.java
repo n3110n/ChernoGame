13a1fc0e77be3048c848595fdede9e437a1c4a62
n3110n <neilon1@web.de>
2018-05-21 01:21:29 +0200

    EP 25
    Random Level generation Basics

--- src/test/level/Level.java
+++ src/test/level/Level.java
@@ -4,8 +4,8 @@ import test.graphics.Screen;
 
 public class Level {
 	
-	private int width, height;
-	private int[] tiles;
+	protected int width, height;
+	protected int[] tiles;
 	
 	public Level(int width, int height) {
 		this.width = width;
@@ -23,7 +23,7 @@ public class Level {
 		
 	}
 	
-	private void generateLevel() {
+	protected void generateLevel() {
 		
 	}
 	
