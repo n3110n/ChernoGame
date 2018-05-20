bf53377e4bfc253bb48c1871c14456b2e651e03a
n3110n <neilon1@web.de>
2018-05-20 19:37:32 +0200

    EP 16
    Self Scrolling Map
    Renamed Variables
    Wrapping map
    pixelwise scrolling

--- src/test/Game.java
+++ src/test/Game.java
@@ -87,8 +87,11 @@ public class Game extends Canvas implements Runnable{
 		}
 	}
 	
+	int x = 0, y = 0;
+	
 	public void update(){
-		
+		x++;
+		y++;
 	}
 	
 	public void render(){
@@ -99,7 +102,7 @@ public class Game extends Canvas implements Runnable{
 		}
 		
 		screen.clear();
-		screen.render();
+		screen.render(x, y);
 		
 		for(int i = 0; i < pixels.length; i++){
 			pixels[i] = screen.pixels[i];
--- src/test/graphics/Screen.java
+++ src/test/graphics/Screen.java
@@ -5,12 +5,11 @@ import java.util.Random;
 public class Screen {
 
 	private int width, height;
-	int map_width = 64;
-	int map_height = 64;
 	//int tileSize = 16; //tilesize (width and height) in pixels on Screen
-	private int map_size = map_width * map_height;
+	public final int MAP_SIZE = 64;
+	public final int MAP_SIZE_MASK = MAP_SIZE - 1;
 	public int[] pixels;
-	public int[] tiles = new int[map_size];
+	public int[] tiles = new int[MAP_SIZE * MAP_SIZE];
 	
 	private Random random = new Random();
 	
@@ -19,17 +18,21 @@ public class Screen {
 		this.height = height;
 		pixels = new int[width*height];
 		
-		for(int i = 0; i < map_size; i++) {
+		for(int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
 			tiles[i] = random.nextInt(0xffffff);
 		}
 	}
 	
-	public void render() {
+	public void render(int xOffset, int yOffset) {
 		for(int y = 0; y < height; y++){
-			if(y < 0 || y >= height) break; //prevent out of bounds
+			int yy = y + yOffset;
+			//if(yy < 0 || yy >= height) break; //prevent out of bounds
 			for(int x = 0; x < width; x++){
-				if(x < 0 || x >= width) break; // prevent out of bounds
-				int tileIndex = (x >> 4) + (y >> 4) * map_width; // >> right shift by number on the right, divided by 2 ^x  bitwise operation
+				int xx = x + xOffset;
+				//if(xx < 0 || xx >= width) break; // prevent out of bounds
+				int tileIndex = ((xx >> 4) & MAP_SIZE_MASK) + ((yy >> 4) & MAP_SIZE_MASK) * MAP_SIZE; 
+				// >> right shift by number on the right, divided by 2 ^x  bitwise operation
+				
 				pixels[x + y * width] = tiles[tileIndex];	
 				
 			}
