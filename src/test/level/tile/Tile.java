eff13e304ab67a058f745ad12bef4203a0b0236b
n3110n <neilon1@web.de>
2018-05-21 13:34:36 +0200

    EP 26
    Tile class created with basic functions

--- src/test/level/tile/Tile.java
+++ src/test/level/tile/Tile.java
@@ -1,7 +1,23 @@
 package test.level.tile;
 
+import test.graphics.Sprite;
+
 public class Tile {
 	
+	public int x, y;
+	public Sprite sprite;
 	
+	public Tile(Sprite sprite) {
+		this.sprite = sprite;
+		
+	}
+	
+	public void render(int x, int y, Screen screen) {
+		
+	}
+	
+	public boolean solid() {
+		return false;
+	}
 
 }
