aa73e6390fd4e7ecddc3c976bdb742a9beacfea7
n3110n <neilon1@web.de>
2018-05-21 22:58:17 +0200

    EP 32
    getting the right tile

--- src/test/level/Level.java
+++ src/test/level/Level.java
@@ -1,6 +1,7 @@
 package test.level;
 
 import test.graphics.Screen;
+import test.level.tile.Tile;
 
 public class Level {
 	
@@ -36,5 +37,11 @@ public class Level {
 		int x1 = (xScroll + screen.width) >> 4;
 		int y0 = yScroll >> 4; // >> 4 = / 16
 		int y1 = (yScroll + screen.height) >> 4;
+	}
+	
+	public Tile getTile(int x, int y) {		
+		if(tiles[x + y * width] == 0) return Tile.grass;
+		
+		return null; //dangerous:D
 	}
 }
