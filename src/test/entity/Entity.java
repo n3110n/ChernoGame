32cc779a42cb5aa86d053d21283024c010da14b9
n3110n <neilon1@web.de>
2018-05-22 15:37:00 +0200

    EP 39
    Entity Class creation
    base settings
    ABSTRACT class

---
+++ src/test/entity/Entity.java
@@ -0,0 +1,31 @@
+package test.entity;
+
+import java.util.Random;
+
+import test.graphics.Screen;
+import test.level.Level;
+
+public abstract class Entity {
+	
+	public int x, y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update() {
+		
+	}
+	
+	public void render(Screen screen) {
+		
+	}
+	
+	public void remove() {
+		//Remove from level later
+		removed = true;
+	}
+
+	public boolean isRemoved() {
+		return removed;
+	}
+}
