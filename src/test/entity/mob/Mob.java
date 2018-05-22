3c961710f727a131e6ae743b9e354ad7a8ba7370
n3110n <neilon1@web.de>
2018-05-22 16:45:53 +0200

    EP 42 movement basics

--- src/test/entity/mob/Mob.java
+++ src/test/entity/mob/Mob.java
@@ -6,10 +6,21 @@ import test.graphics.Sprite;
 public abstract class Mob extends Entity{
 	
 	protected Sprite sprite;
-	protected int direction = 0;
+	protected int dir = 0; //direction
 	protected boolean moving = false;
 	
-	public void move() {
+	public void move(int xa, int ya) {
+		// 0 north, 1 east, 2 south, 3 west
+		if(xa > 0) dir = 1;
+		if(xa < 0) dir = 3;
+		if(ya > 0) dir = 2;
+		if(ya < 0) dir = 0;
+		
+		// -1, 0, 1
+		if(!collision()) {
+			x += xa;
+			y += ya;
+		}
 		
 	}
 	
--- src/test/entity/mob/Player.java
+++ src/test/entity/mob/Player.java
@@ -1,5 +1,23 @@
 package test.entity.mob;
 
-public class Player {
+public class Player extends Mob {
+	
+	
+	public Player() {
+		
+	}
 
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
 }
