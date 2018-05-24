bf550dcbdb599df686eda883030cd40c7cb94ceb
n3110n <neilon1@web.de>
2018-05-24 15:59:37 +0200

    EP 62 reload

--- src/test/level/TileCoordinate.java
+++ src/test/level/TileCoordinate.java
@@ -2,26 +2,31 @@ package test.level;
 
 public class TileCoordinate {
 	
-	private int x,y;
+	private double x,y;
 	private final int TILE_SIZE = 16;
 	
 	public TileCoordinate(int x, int y) {
+		this.x = (double)x * TILE_SIZE;
+		this.y = (double)y * TILE_SIZE; 	
+		}
+	
+	public TileCoordinate(double x, double y) {
 		this.x = x * TILE_SIZE;
 		this.y = y * TILE_SIZE; 	
 		}
 	
 	public int x() {
-		return x;
+		return (int)x;
 	}
 	
 	public int y() {
-		return y;
+		return (int)y;
 	}
 	
 	public int[] xy() {
 		int temp[] = new int[2];
-		temp[0] = x;
-		temp[1] = y;
+		temp[0] = (int)x;
+		temp[1] = (int)y;
 		return temp;
 	}
 
