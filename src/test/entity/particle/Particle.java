1f45299a8afc8d884fb3574bcaaab7af7c392415
n3110n <neilon1@web.de>
2018-06-14 11:04:31 +0200

    Revert "fil"
    This reverts commit 4caac03fd3c1a1473b4b05b5b0ce94225efb5890.

--- src/test/entity/particle/Particle.java
+++ src/test/entity/particle/Particle.java
@@ -1,6 +1,5 @@
 package test.entity.particle;
 
-import test.Game;
 import test.entity.Entity;
 import test.graphics.Screen;
 import test.graphics.Sprite;
@@ -24,11 +23,12 @@ public class Particle extends Entity{
 		this.life = life / 2 + random.nextInt(life);
 		sprite = Sprite.particle_normal;
 		
-		this.xa = random.nextGaussian();
+		this.xa = random.nextGaussian() + 1.8;
 		this.ya = random.nextGaussian();
 		this.zOffset = random.nextFloat() + 2.0;
 		this.zz = zOffset;
 		
+		if(this.xa < 0) xa = 0.1;
 		
 	}
 	
@@ -45,54 +45,13 @@ public class Particle extends Entity{
 			ya *= 0.4;
 		}
 	
-		move(xx + xa, yy + ya + zz + za);
-	}
-
-	public boolean collision(double x, double y, Sprite sprite) {  // x Offset from the right, y Offset from top
-		boolean solid = false;
-		
-		for(int yi = 0; yi < sprite.getHeight(); yi++) {
-			for(int xi = 0; xi < sprite.getWidth(); xi++) {
-				if(sprite.pixels[yi * sprite.getWidth() + xi] != 0xffff00ff && level.getTile((int)((x + xi + 1) / 16), ((int)(y + yi - 1) / 16)).getSolid())  return true;
-			
-			}
-		}
-		return solid;
-	}
-	
-	public boolean collision(double x, double y) {  // x Offset from the right, y Offset from top
-		boolean solid = false;
-		
-		for(int c = 0; c < 4; c++) {			
-			double xt = (x - c % 2 * 16);
-			double yt = (y + c / 2 * 16);
-			int xi = (int) Math.ceil(xt / Game.TILE_SIZE);
-			int yi = (int) Math.ceil(yt / Game.TILE_SIZE);
-			if(level.getTile(xi, yi).getSolid()) {
-				solid = true;
-			}
-		}
-		return solid;
-	}
-	
-	private void move(double x, double y) {
-		
-		if(collision(x, y)) {
-			this.xa *= -.5;
-			this.ya *= -.5;
-			this.za *= -.5;
-		}
-		
 		this.xx += xa;
 		this.yy += ya;
 		this.zz += za;
-		
-
 	}
-
-
+	
 	public void render(Screen screen) {
-		screen.renderSprite((int)xx, (int)yy - (int)zz + (int) zOffset, sprite, true);
+		screen.renderSprite((int)xx - 4, (int)yy - (int)zz + (int) zOffset, sprite, true);
 		
 	}
 }
